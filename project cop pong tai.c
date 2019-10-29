95f9a504ea0416322acd0ff51dac3a543671c7c3
maxkittitad <52563334+maxkittitad@users.noreply.github.com>
2019-10-29 10:54:50 +0700

    Add files via upload

---
+++ project cop pong tai.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    printf("Welcome to the Bubble Tea\n");
+    printf("Menu                         Price\n");
+    printf("0: Black Tea                 30\n");
+    printf("1: Green Tea                 30\n");
+    printf("2: Thai Tea                  30\n");
+    printf("3: Passion Fruit Green Tea   45\n");
+    printf("4: Taro Smoothie Tea         40\n");
+    printf("5: Peach Tea                 35\n");
+}
